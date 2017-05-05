b27152ab92ff47958ccc9fadc14f626841e9eb5b
nicolas.coulonnier <nicolas.coulonnier@epsi.fr>
2017-05-05 11:46:12 +0200

    Correction servlet2

--- src/fr/epsi/myEpsi/servlet/VersionServlet.java
+++ src/fr/epsi/myEpsi/servlet/VersionServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class VersionServelet
  */
-@WebServlet("/VersionServelet")
+@WebServlet("/VersionServlet")
 public class VersionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
